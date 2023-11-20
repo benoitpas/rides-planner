bd090f30cd951db61e32317ffc00e70306d6c80c
benoitpas <bpasquereau@gmail.com>
2023-11-20 20:26:10 +0000

    initial fold skeleton

--- extract-segments/src/main/scala/Main.scala
+++ extract-segments/src/main/scala/Main.scala
@@ -10,8 +10,20 @@ def main(args:Array[String]): Unit =
     val route1 = routes.head
     val route2 = routes.tail.head
     println(s"overlap between the 2 routes:${route1._1.overlap(route2._1)}")
+
+    case class State(ol_seg:Set[Segment], ol_points:List[Point], nol_seg:Set[Segment], nol_points:List[Point])
+
+    def next(s:State, p:Point):State =
+      val pc = route2.findClosest(p)
+      val isClose = pc._1 <1e-9
+      (isClose, p, s.ol_points,s.nol_points) match
+        case (true, _, _, List()) => State(s.ol_seg,p::s.ol_points, s.nol_seg,s.nol_points)
+
     for(p <- route1._2)
       val pc = route2.findClosest(p)
       println(s"${p}\t${pc}")
+
+    val r= route2._2.foldLeft(State(Set(),List(),Set(),List()))(next)
+    println(r)
 
   }
