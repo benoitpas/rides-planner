045c2a831ee2729a1124250f5a32bd73ce2e1ef4
benoitpas <bpasquereau@gmail.com>
2023-11-21 07:16:15 +0000

    Added more statuses

--- extract-segments/src/main/scala/Main.scala
+++ extract-segments/src/main/scala/Main.scala
@@ -11,19 +11,27 @@ def main(args:Array[String]): Unit =
     val route2 = routes.tail.head
     println(s"overlap between the 2 routes:${route1._1.overlap(route2._1)}")
 
+    // ol => overlaping points/segments
+    // nol => non-overlapping points/segments
     case class State(ol_seg:Set[Segment], ol_points:List[Point], nol_seg:Set[Segment], nol_points:List[Point])
 
     def next(s:State, p:Point):State =
       val pc = route2.findClosest(p)
-      val isClose = pc._1 <1e-9
-      (isClose, p, s.ol_points,s.nol_points) match
-        case (true, _, _, List()) => State(s.ol_seg,p::s.ol_points, s.nol_seg,s.nol_points)
+      val isClose = pc._1 < 1e-7
+      (isClose, s.ol_points,s.nol_points) match
+        case (false, List(), _) => State(s.ol_seg,s.ol_points, s.nol_seg,p::s.nol_points)
+        case (false, _, _) => State(s.ol_seg + Segment.fromPoints(s.ol_points),List(), s.nol_seg,List(p))
+        case (true,  _, List()) => State(s.ol_seg,p::s.ol_points, s.nol_seg,s.nol_points)
+        case (true,  _, _) => State(s.ol_seg,List(p), s.nol_seg + Segment.fromPoints(s.nol_points),List())
 
     for(p <- route1._2)
       val pc = route2.findClosest(p)
-      println(s"${p}\t${pc}")
+      println(s"${p}\t${pc._1 < 1e-7}\t${pc}")
 
-    val r= route2._2.foldLeft(State(Set(),List(),Set(),List()))(next)
-    println(r)
+    val r = route1._2.foldLeft(State(Set(),List(),Set(),List()))(next)
+    val ol_seg = Segment.addToSet(r.ol_points, r.ol_seg)
+    val nol_seg = Segment.addToSet(r.nol_points, r.nol_seg)
+    println(ol_seg.size)
+    println(nol_seg.size)
 
   }
